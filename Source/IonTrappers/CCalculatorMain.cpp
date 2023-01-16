9c8c4b720939c59385b40a1cccee16d7166eb457
Radlowsky <56251943+Radlowsky@users.noreply.github.com>
2023-01-16 21:46:50 +0100

    Update CCalculatorMain.cpp

--- Source/IonTrappers/CCalculatorMain.cpp
+++ Source/IonTrappers/CCalculatorMain.cpp
@@ -104,7 +104,7 @@ namespace {
 
 
 
-//int UCCalculatorMain::CalculateMatrix(float phase, float& posibility)
+
 TArray<float> UCCalculatorMain::CalculateMatrix(TArray<float>values, int gateID, float phase, TArray<float>parameters)
 
 {
@@ -114,7 +114,7 @@ TArray<float> UCCalculatorMain::CalculateMatrix(TArray<float>values, int gateID,
     int tmp = 3;
     MatrixCalculator hadamardGate(2, 2, std::vector<std::vector<std::complex<double>>>{ {1 / sqrt(2), 1 / sqrt(2)}, { 1 / sqrt(2), -1 / sqrt(2) } });
     MatrixCalculator phaseGate(2, 2, std::vector<std::vector<std::complex<double>>>{ {1, 0}, { 0, exp(pH * 1i) } });
-    //Matrix mult(2, 2, std::vector<std::vector<std::complex<double>>>{ {0, 0}, {0, 0} });
+
     MatrixCalculator slope(2, 1, std::vector<std::vector<std::complex<double>>>{ {1}, { 0 } });
     MatrixCalculator slope2(2, 1, std::vector<std::vector<std::complex<double>>>{ {1}, { 0 } });
     MatrixCalculator controlledPhaseGate(4, 4, std::vector<std::vector<std::complex<double>>>{ {1, 0, 0, 0}, { 0,1,0,0 }, { 0,0,1,0 }, { 0,0,0,pH } });
@@ -124,7 +124,7 @@ TArray<float> UCCalculatorMain::CalculateMatrix(TArray<float>values, int gateID,
     MatrixCalculator res(2, 1);
     MatrixCalculator res2(4, 1);
     MatrixCalculator tmpMatrix(2, 1);
-    //double imag = res[0][1].imag();
+
         res2[0][0].real(values[1]);
         res2[0][0].imag(values[2]);
         res2[1][0].real(values[3]);
@@ -146,65 +146,42 @@ TArray<float> UCCalculatorMain::CalculateMatrix(TArray<float>values, int gateID,
 
         bool resUsed = false;
 
-     UE_LOG(LogTemp, Warning, TEXT("%f"), values[0]);
-     UE_LOG(LogTemp, Warning, TEXT("liczba bramek: %d"), values.Num());
-     //UE_LOG(LogTemp, Warning, TEXT("%f"), 1-oneProbability[0]);
-    //Algo::Reverse(GatesIDs);
+
         switch (gateID) {
         case 1:
                 res = hadamardGate * res; 
                 resUsed = true;
-                UE_LOG(LogTemp, Warning, TEXT("Hadamard"));
+
             break;
         case 2:
                 res = phaseGate * res;
                 resUsed = true;
-                UE_LOG(LogTemp, Warning, TEXT("Fazy"));
+
             break;
         case 3:
                 tensorProduct = tmpMatrix.tensorProduct(res);
                 tensorProduct = controlledqNotGate * tensorProduct;
-                UE_LOG(LogTemp, Warning, TEXT("CNOT"));
                 return calculateDistribution(tensorProduct, res2, values);
             break;
         case 4:
                 res2 = qNotGate * res2;
-                UE_LOG(LogTemp, Warning, TEXT("NOT"));
             break;        
         default:
             break;
         }
-        UE_LOG(LogTemp, Warning, TEXT("00: %f"), res[0][0].real());
-        UE_LOG(LogTemp, Warning, TEXT("01: %f"), res[0][1].imag());
-        UE_LOG(LogTemp, Warning, TEXT("11: %f"), res[1][1].real());
-        UE_LOG(LogTemp, Warning, TEXT("10: %f"), res[1][0].imag());
-
-    //slope = h * slope;
-
-    //slope2 = qnot * slope2;
-
-    //tp = slope2.tensorProduct(slope);
-
-    //tp = cp * tp;
-
-    //auto phase = log(tp[3][0] / tp[1][0].real()).imag();
-    //auto prob = tp;
-    //UE_LOG(LogTemp, Warning, TEXT("%f"), phase);
-       
 
 
     const int nrolls = 10000; // number of experiments
     std::default_random_engine generator;
     std::bernoulli_distribution distribution(pow(abs(res[1][0]), 2));
-    //std::bernoulli_distribution distribution(pow(abs(tp[1][0]), 2));
+
     float posibility = 0;
 
     std::srand(time(NULL));
     generator.seed(rand());
 
     for (int i2 = 0; i2 < nrolls; ++i2) if (distribution(generator)) ++posibility;
-    posibility = 1 - posibility/10000;
-    UE_LOG(LogTemp, Warning, TEXT("%f"), posibility);
+
 
     if (resUsed) {
         res2[0][0].real(res[0][0].real());
@@ -225,24 +202,3 @@ TArray<float> UCCalculatorMain::CalculateMatrix(TArray<float>values, int gateID,
 
 	return returnValues;
 }
-
-
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[0][0].real(), res[0][0].imag());
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[1][0].real(), res[1][0].imag());
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[1][1].real(), res[1][1].imag());
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[0][1].real(), res[0][1].imag());
-////res = phaseGate * res;
-//res[0][0].real(0.707107);
-//res[0][0].imag(0);
-//res[1][0].real(0.5);
-//res[1][0].imag(0.5);
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[0][0].real(), res[0][0].imag());
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[1][0].real(), res[1][0].imag());
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[1][1].real(), res[1][1].imag());
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[0][1].real(), res[0][1].imag());
-//res = hadamardGate * res;
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[0][0].real(), res[0][0].imag());
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[1][0].real(), res[1][0].imag());
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[1][1].real(), res[1][1].imag());
-//UE_LOG(LogTemp, Warning, TEXT("Real: %f Img: %f"), res[0][1].real(), res[0][1].imag());
-//UE_LOG(LogTemp, Warning, TEXT("Hadamard"));
